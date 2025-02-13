86f840e52e6f440feca1b41760861b2094ab20c6
Christopher Malebrán <christopher-mr@hotmail.es>
2025-02-13 11:51:24 -0300

    Fix provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, RouterOutlet } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';  // Para el servicio HTTP
 import { routes } from './app.routes';
-import { PathLocationStrategy } from '@angular/common';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    PathLocationStrategy,
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(), // Proveedor de HttpClientModule
